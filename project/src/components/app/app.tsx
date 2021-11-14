62fb4ab0fb5f1f59de47a9e698b975d4f3512f28
Andrei <i.andrei.abramov@gmail.com>
2021-11-14 14:22:40 +0300

    LoginPage component and route to it created

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from '../home-page/home-page';
+import LoginPage from '../login-page/login-page';
 
 function App(): JSX.Element {
   return (
     <Routes>
       <Route path='/' element={ <HomePage /> } />
+      <Route path='/login' element={ <LoginPage /> } />
     </Routes>
   );
 }
