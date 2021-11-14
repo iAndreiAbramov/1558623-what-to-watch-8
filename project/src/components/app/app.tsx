7712391838bc208cdfe6e53eed8d74d4f052388e
Andrei <i.andrei.abramov@gmail.com>
2021-11-14 15:51:27 +0300

    AddReviewPage component and route to it created

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -5,6 +5,7 @@ import LoginPage from '../login-page/login-page';
 import MyList from '../my-list/my-list';
 import FilmPage from '../film-page/film-page';
 import Player from '../player/player';
+import AddReviewPage from '../add-review-page/add-review-page';
 
 function App(): JSX.Element {
   return (
@@ -14,6 +15,7 @@ function App(): JSX.Element {
       <Route path='/mylist' element={ <MyList /> } />
       <Route path='/films/:id' element={ <FilmPage /> } />
       <Route path='/player/:id' element={ <Player /> } />
+      <Route path='/films/:id/review' element={ <AddReviewPage /> } />
     </Routes>
   );
 }
