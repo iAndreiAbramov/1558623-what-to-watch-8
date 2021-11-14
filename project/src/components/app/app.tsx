0dd5440cb946582144796b64e1bb48e46601d331
Andrei <i.andrei.abramov@gmail.com>
2021-11-14 15:21:07 +0300

    Player component and a route to it created

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -4,6 +4,7 @@ import HomePage from '../home-page/home-page';
 import LoginPage from '../login-page/login-page';
 import MyList from '../my-list/my-list';
 import FilmPage from '../film-page/film-page';
+import Player from '../player/player';
 
 function App(): JSX.Element {
   return (
@@ -12,6 +13,7 @@ function App(): JSX.Element {
       <Route path='/login' element={ <LoginPage /> } />
       <Route path='/mylist' element={ <MyList /> } />
       <Route path='/films/:id' element={ <FilmPage /> } />
+      <Route path='/player/:id' element={ <Player /> } />
     </Routes>
   );
 }
