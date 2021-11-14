0ba276eec1a27de3919e3c824139109ee9ed0810
Andrei <i.andrei.abramov@gmail.com>
2021-11-14 14:29:33 +0300

    MyList component and path to it created

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import HomePage from '../home-page/home-page';
 import LoginPage from '../login-page/login-page';
+import MyList from '../my-list/my-list';
 
 function App(): JSX.Element {
   return (
     <Routes>
       <Route path='/' element={ <HomePage /> } />
       <Route path='/login' element={ <LoginPage /> } />
+      <Route path='/mylist' element={ <MyList /> } />
     </Routes>
   );
 }
