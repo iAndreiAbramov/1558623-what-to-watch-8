6a0b33705d2373a55addda8f7982f4a8b3276a54
Andrei <i.andrei.abramov@gmail.com>
2021-11-25 02:46:36 +0300

    Tests for player reducer created

---
+++ project/src/store/reducers/player-reducer.test.ts
@@ -0,0 +1,44 @@
+import { ActionType } from '../action-creators';
+import { playerReducer, PlayerStateTypes } from './player-reducer';
+
+describe('Reducer playerReducer', () => {
+  const initialState: PlayerStateTypes = {
+    playerData: {
+      videoLink: '',
+    },
+  };
+
+  it('should not update state if action type is "Unknown"', () => {
+    expect(playerReducer(
+      initialState,
+      {
+        type: ActionType.Unknown,
+        payload: 'fake-string',
+      }
+    )).toEqual(initialState);
+  });
+
+  it('should update state correctly if action type is "setPlayerDataAction"', () => {
+    const expectedState = Object.assign(
+      {},
+      initialState,
+      {
+        playerData: {
+          videoLink: 'fake-string',
+        },
+      },
+    );
+
+    expect(playerReducer(
+      initialState,
+      {
+        type: ActionType.SetPlayerData,
+        payload: {
+          playerData: {
+            videoLink: 'fake-string',
+          },
+        },
+      },
+    )).toEqual(expectedState);
+  });
+});
