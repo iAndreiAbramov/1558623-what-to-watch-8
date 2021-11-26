4c76f06de5900317232ae699ce2a5fc368449ea3
Andrei <i.andrei.abramov@gmail.com>
2021-11-26 21:57:54 +0300

    BreadCrumbs test supplemented

--- project/src/components/breadcrumbs/breadcrumbs.test.tsx
+++ project/src/components/breadcrumbs/breadcrumbs.test.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import * as Redux from 'react-redux';
 import { Provider, useSelector } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { configureMockStore } from '@jedmao/redux-mock-store';
 import { render, screen } from '@testing-library/react';
+import thunk from 'redux-thunk';
 import userEvent from '@testing-library/user-event';
 import { AuxProps } from '../../store/selectors.test';
 import Breadcrumbs from './breadcrumbs';
 import { getCurrentFilmData } from '../../store/selectors';
 import { mockStoreWithAuth } from '../../mocks/store-mocks';
 import { renderHook } from '@testing-library/react-hooks';
+import { AppRoute } from '../../const';
+import FilmPage from '../film-page/film-page';
+import { createApi } from '../../services/api';
 
 describe('Component BreadCrumbs', () => {
-  const mockStore = configureMockStore();
+  const onFakeUnauthorized = jest.fn();
+  const api = createApi(onFakeUnauthorized);
+  const middlewares = [thunk.withExtraArgument(api)];
+  const mockStore = configureMockStore(middlewares);
   const store = mockStore(mockStoreWithAuth);
   const fakeApp = (
     <Provider store={ store }>
@@ -40,6 +47,29 @@ describe('Component BreadCrumbs', () => {
     render(fakeApp);
 
     userEvent.click(screen.getByRole('link'));
+    expect(dispatch).toBeCalledTimes(1);
+  });
+
+  it('should redirect to film page and dispatch one action on click', () => {
+    const dispatch = jest.fn();
+    const useDispatch = jest.spyOn(Redux, 'useDispatch');
+    useDispatch.mockReturnValue(dispatch);
+    window.history.pushState(null, '', '/fake');
+    render(
+      <Provider store={ store }>
+        <BrowserRouter window={ window }>
+          <Routes>
+            <Route path="/fake" element={ <Breadcrumbs /> } />
+            <Route path={ AppRoute.Film }>
+              <Route path=':id' element={ <h1>Film page</h1>} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>);
+
+    expect(screen.queryByText('Film page')).not.toBeInTheDocument();
+    userEvent.click(screen.getByRole('link'));
+    expect(screen.getByText('Film page')).toBeInTheDocument();
     expect(dispatch).toBeCalledTimes(1);
   });
 });
