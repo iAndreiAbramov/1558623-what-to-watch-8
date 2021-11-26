2b1e1733b680cf24143df217ab767117f01c12b4
Andrei <i.andrei.abramov@gmail.com>
2021-11-26 23:42:02 +0300

    Test for PosterBackground created

---
+++ project/src/components/poster-background/poster-background.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import PosterBackground from './poster-background';
+
+describe('Component PosterBackground', () => {
+  it('should render correctly', () => {
+    render(<PosterBackground backgroundImage={ 'test-bg' } name={ 'test-name' } />);
+    expect(screen.getByAltText('test-name')).toBeInTheDocument();
+  });
+});
